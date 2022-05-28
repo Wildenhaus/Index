3dfae2d47d34c8d78e6ee8397f47a58a4497018e
Haus <reid@wilden.haus>
2022-05-27 23:49:57 -0400

    Resolved an unknown for S3DAnimTrackSerializer

--- Reclaimer.Saber3D/Serializers/S3DAnimTrackSerializer.cs
+++ Reclaimer.Saber3D/Serializers/S3DAnimTrackSerializer.cs
@@ -11,7 +11,7 @@ namespace Saber3D.Serializers
 
     protected override void OnDeserialize( BinaryReader reader, S3DAnimTrack animTrack )
     {
-      var unk_01 = reader.ReadUInt32();
+      var propertyCount = reader.ReadUInt32();
       var propertyFlags = reader.ReadBitArray( 4 );
 
       if ( propertyFlags[ 0 ] )
@@ -38,15 +38,6 @@ namespace Saber3D.Serializers
 
     private void ReadObjMapListProperty( BinaryReader reader, S3DAnimTrack animTrack )
     {
-      //// TODO: No idea what this is. Maybe a bitfield? RTTI says its a Vector<Vector<short,8>>
-      //// This is similar to the 0x0300000007 in S3DObjectGeometryUnshared
-      //// Might be a bitfield? Maybe length is (0xC * ObjAnimList.Count)?
-      //var unk_01 = reader.ReadUInt32();
-      //var unk_02 = reader.ReadByte();
-      //Assert( unk_01 == 0x03 );
-      //Assert( unk_02 == 0x05 );
-      //for ( var i = 0; i < 0xC; i++ )
-      //  reader.ReadByte();
       throw new NotImplementedException();
     }
 
