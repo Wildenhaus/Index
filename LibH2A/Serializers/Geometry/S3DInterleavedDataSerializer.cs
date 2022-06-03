ba486e2c0124e956dd367b2a6fa53f979304d68f
Haus <reid@wilden.haus>
2022-06-03 14:19:11 -0400

    GeometryBufferFlags layout/mapping fixes

--- LibH2A/Serializers/Geometry/S3DInterleavedDataSerializer.cs
+++ LibH2A/Serializers/Geometry/S3DInterleavedDataSerializer.cs
@@ -51,7 +51,14 @@ namespace Saber3D.Serializers.Geometry
       //  System.Diagnostics.Debugger.Break();
 
       ReadTangents( reader, data );
-      ReadColors( reader, data );
+
+      if ( Flags.HasFlag( S3DGeometryBufferFlags.Unk_17 ) )
+        _ = reader.ReadInt32();
+      if ( Flags.HasFlag( S3DGeometryBufferFlags.Unk_18 ) )
+        _ = reader.ReadInt32();
+      if ( Flags.HasFlag( S3DGeometryBufferFlags.Unk_19 ) )
+        _ = reader.ReadInt32();
+
       ReadUVs( reader, data );
 
       var readSize = reader.BaseStream.Position - startPos;
@@ -79,64 +86,31 @@ namespace Saber3D.Serializers.Geometry
 
     private Vector4 ReadTangent( BinaryReader reader )
     {
-      // TODO: This is a guess. Verify.
-      if ( Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TANG ) )
-      {
-        // 0x2 bytes
-        _ = reader.ReadInt16();
-        return default;
-      }
-      else
-      {
-        var x = reader.ReadSByte().SNormToFloat();
-        var y = reader.ReadSByte().SNormToFloat();
-        var z = reader.ReadSByte().SNormToFloat();
-        var w = reader.ReadSByte().SNormToFloat();
+      var x = reader.ReadSByte().SNormToFloat();
+      var y = reader.ReadSByte().SNormToFloat();
+      var z = reader.ReadSByte().SNormToFloat();
+      var w = reader.ReadSByte().SNormToFloat();
 
-        Assert( x >= -1.01 && x <= 1.01, "Tangent X coord out of bounds." );
-        Assert( y >= -1.01 && y <= 1.01, "Tangent Y coord out of bounds." );
-        Assert( z >= -1.01 && z <= 1.01, "Tangent Z coord out of bounds." );
-        Assert( w >= -1.01 && w <= 1.01, "Tangent W coord out of bounds." );
+      Assert( x >= -1.01 && x <= 1.01, "Tangent X coord out of bounds." );
+      Assert( y >= -1.01 && y <= 1.01, "Tangent Y coord out of bounds." );
+      Assert( z >= -1.01 && z <= 1.01, "Tangent Z coord out of bounds." );
+      Assert( w >= -1.01 && w <= 1.01, "Tangent W coord out of bounds." );
 
-        return new Vector4( x, y, z, w );
-      }
-    }
-
-    private void ReadColors( BinaryReader reader, S3DInterleavedData data )
-    {
-      if ( Flags.HasFlag( S3DGeometryBufferFlags._COL0 ) )
-        data.Color0 = ReadColor( reader );
-      if ( Flags.HasFlag( S3DGeometryBufferFlags._COL1 ) )
-        data.Color1 = ReadColor( reader );
-      if ( Flags.HasFlag( S3DGeometryBufferFlags._COL2 ) )
-        data.Color2 = ReadColor( reader );
-      if ( Flags.HasFlag( S3DGeometryBufferFlags._COL3 ) )
-        data.Color3 = ReadColor( reader );
-      if ( Flags.HasFlag( S3DGeometryBufferFlags._COL4 ) )
-        data.Color4 = ReadColor( reader );
-    }
-
-    private Vector4 ReadColor( BinaryReader reader )
-    {
-      // 0x1 bytes
-      //var col = reader.ReadByte();
-      return default;
+      return new Vector4( x, y, z, w );
     }
 
     private void ReadUVs( BinaryReader reader, S3DInterleavedData data )
     {
       if ( Flags.HasFlag( S3DGeometryBufferFlags._TEX0 ) /*|| Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_0 )*/ )
-        data.UV0 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_0 ), Flags.HasFlag( S3DGeometryBufferFlags._TEX0_4D ) );
+        data.UV0 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_0 ) );
       if ( Flags.HasFlag( S3DGeometryBufferFlags._TEX1 ) /*|| Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_1 )*/ )
-        data.UV1 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_1 ), Flags.HasFlag( S3DGeometryBufferFlags._TEX1_4D ) );
+        data.UV1 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_1 ) );
       if ( Flags.HasFlag( S3DGeometryBufferFlags._TEX2 ) /*|| Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_2 )*/ )
-        data.UV2 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_2 ), Flags.HasFlag( S3DGeometryBufferFlags._TEX2_4D ) );
+        data.UV2 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_2 ) );
       if ( Flags.HasFlag( S3DGeometryBufferFlags._TEX3 ) /*|| Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_3 )*/ )
-        data.UV3 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_3 ), Flags.HasFlag( S3DGeometryBufferFlags._TEX3_4D ) );
+        data.UV3 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_3 ) );
       if ( Flags.HasFlag( S3DGeometryBufferFlags._TEX4 ) /*|| Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_4 )*/ )
-        data.UV4 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_4 ), Flags.HasFlag( S3DGeometryBufferFlags._TEX4_4D ) );
-      if ( Flags.HasFlag( S3DGeometryBufferFlags._TEX5 ) /*|| Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_5 ) */)
-        data.UV5 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_5 ), Flags.HasFlag( S3DGeometryBufferFlags._TEX5_4D ) );
+        data.UV4 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_4 ) );
 
       //if ( Flags.HasFlag( S3DGeometryBufferFlags._TEX0 ) || Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_0 ) )
       //  data.UV0 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_0 ), Flags.HasFlag( S3DGeometryBufferFlags._TEX0_4D ) );
@@ -152,44 +126,31 @@ namespace Saber3D.Serializers.Geometry
       //  data.UV5 = ReadUV( reader, Flags.HasFlag( S3DGeometryBufferFlags._COMPRESSED_TEX_5 ), Flags.HasFlag( S3DGeometryBufferFlags._TEX5_4D ) );
     }
 
-    private Vector4 ReadUV( BinaryReader reader, bool isCompressed, bool is4D )
+    private Vector4 ReadUV( BinaryReader reader, bool isCompressed )
     {
-      if ( is4D )
+      if ( isCompressed )
       {
-        // 7 bytes
-        //_ = reader.ReadUInt16();
-        //_ = reader.ReadUInt16();
-        //_ = reader.ReadUInt16();
-        //_ = reader.ReadByte();
-        //return default;
-        return FailReturn<Vector4>( "4D UV Not implemented" );
+        // Fairly sure this is correct
+        // Broken position?
+        var u = reader.ReadInt16().SNormToFloat();
+        var v = 1 - reader.ReadInt16().SNormToFloat();
+
+        //Assert( u >= 0 && u <= 1, "UV U coord out of bounds." );
+        //Assert( v >= 0 && v <= 1, "UV V coord out of bounds." );
+
+        return new Vector4( u, v, 0, 0 );
       }
       else
       {
-        if ( isCompressed )
-        {
-          // Fairly sure this is correct
-          // Broken position?
-          var u = reader.ReadInt16().SNormToFloat();
-          var v = 1 - reader.ReadInt16().SNormToFloat();
+        // Fairly sure this is correct
+        // Broken position?
+        var u = reader.ReadSingle();
+        var v = 1 - reader.ReadSingle();
 
-          //Assert( u >= 0 && u <= 1, "UV U coord out of bounds." );
-          //Assert( v >= 0 && v <= 1, "UV V coord out of bounds." );
+        //Assert( u >= -2 && u <= 2, "UV U coord out of bounds." );
+        //Assert( v >= -2 && v <= 2, "UV V coord out of bounds." );
 
-          return new Vector4( u, v, 0, 0 );
-        }
-        else
-        {
-          // Fairly sure this is correct
-          // Broken position?
-          var u = reader.ReadInt16().SNormToFloat();
-          var v = 1 - reader.ReadInt16().SNormToFloat();
-
-          //Assert( u >= -2 && u <= 2, "UV U coord out of bounds." );
-          //Assert( v >= -2 && v <= 2, "UV V coord out of bounds." );
-
-          return new Vector4( u, v, 0, 0 );
-        }
+        return new Vector4( u, v, 0, 0 );
       }
     }
 
