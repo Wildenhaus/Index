8c93b29183cb1c731fe78599f1dd31999f6dbe16
Haus <reid@wilden.haus>
2022-06-04 11:41:20 -0400

    First sentinel section on S3DMesh is a 64-bit flags section

--- LibH2A/Data/S3DGeometryMesh.cs
+++ LibH2A/Data/S3DGeometryMesh.cs
@@ -6,8 +6,10 @@ namespace Saber3D.Data
   public class S3DGeometryMesh
   {
     public uint Id { get; set; }
-    public ushort Unk_01 { get; set; } // TODO
-    public S3DMeshFlags Flags { get; set; } // TODO: This is a guess
+
+    public ushort FlagCount { get; set; }
+    public S3DGeometryMeshFlags Flags { get; set; }
+
     public S3DGeometryMeshBuffer[] Buffers { get; set; }
 
   }
@@ -18,43 +20,72 @@ namespace Saber3D.Data
     public uint SubBufferOffset;
   }
 
-  // TODO
-  /* These are just guesses based on what I've found in the disassembly.
-   * At a glance, they don't appear to be correct though.
-   * Figuring these out would make reading/converting the mesh data
-   * a lot cleaner.
-   */
   [Flags]
-  public enum S3DMeshFlags : ulong
+  public enum S3DGeometryMeshFlags : ulong
   {
-    Unk_01 = 1 << 0x01,
-    Unk_02 = 1 << 0x02,
-    Unk_03 = 1 << 0x03,
-    Unk_04 = 1 << 0x04,
-    Unk_05 = 1 << 0x05,
-    Unk_06 = 1 << 0x06,
-    Tex0_4D = 1 << 0x07,
-    Tex1_4D = 1 << 0x08,
-    Tex2_4D = 1 << 0x09,
-    Tex3_4D = 1 << 0x0A,
-    Tex4_4D = 1 << 0x0B,
-    Tex5_4D = 1 << 0x0C,
-    Unk_0D = 1 << 0x0D,
-    Norm_In_Vert4 = 1 << 0x0E,
-    Tang0 = 1 << 0x0F,
-    Tang1 = 1 << 0x10,
-    Tang2 = 1 << 0x11,
-    Tang3 = 1 << 0x12,
-    Tang4 = 1 << 0x13,
-    Unk_14 = 1 << 0x14,
-    Unk_15 = 1 << 0x15,
-    Unk_16 = 1 << 0x16,
-    Unk_17 = 1 << 0x17,
-    Unk_18 = 1 << 0x18,
-    Compr_Vert = 1 << 0x19,
-    Compr_Tex = 1 << 0x1A,
-    Compr_Tang = 1 << 0x1B,
-    Compr_Norm = 1 << 0x1C,
+    Unk_01 = 1ul << 0x00,
+    Unk_02 = 1ul << 0x01,
+    Unk_03 = 1ul << 0x02,
+    Unk_04 = 1ul << 0x03,
+    Unk_05 = 1ul << 0x04,
+    Unk_06 = 1ul << 0x05,
+    Unk_07 = 1ul << 0x06,
+    Unk_08 = 1ul << 0x07,
+    Unk_09 = 1ul << 0x08,
+    Unk_0A = 1ul << 0x09,
+    Unk_0B = 1ul << 0x0A,
+    Unk_0C = 1ul << 0x0B,
+    Unk_0D = 1ul << 0x0C,
+    Unk_0E = 1ul << 0x0D,
+    Unk_0F = 1ul << 0x0E,
+    Unk_10 = 1ul << 0x0F,
+    Unk_11 = 1ul << 0x10,
+    Unk_12 = 1ul << 0x11,
+    Unk_13 = 1ul << 0x12,
+    Unk_14 = 1ul << 0x13,
+    Unk_15 = 1ul << 0x14,
+    Unk_16 = 1ul << 0x15,
+    Unk_17 = 1ul << 0x16,
+    Unk_18 = 1ul << 0x17,
+    Unk_19 = 1ul << 0x18,
+    Unk_1A = 1ul << 0x19,
+    Unk_1B = 1ul << 0x1A,
+    Unk_1C = 1ul << 0x1B,
+    Unk_1D = 1ul << 0x1C,
+    Unk_1E = 1ul << 0x1D,
+    Unk_1F = 1ul << 0x1E,
+    Unk_20 = 1ul << 0x1F,
+    Unk_21 = 1ul << 0x20,
+    Unk_22 = 1ul << 0x21,
+    Unk_23 = 1ul << 0x22,
+    Unk_24 = 1ul << 0x23,
+    Unk_25 = 1ul << 0x24,
+    Unk_26 = 1ul << 0x25,
+    Unk_27 = 1ul << 0x26,
+    Unk_28 = 1ul << 0x27,
+    Unk_29 = 1ul << 0x28,
+    Unk_2A = 1ul << 0x29,
+    Unk_2B = 1ul << 0x2A,
+    Unk_2C = 1ul << 0x2B,
+    Unk_2D = 1ul << 0x2C,
+    Unk_2E = 1ul << 0x2D,
+    Unk_2F = 1ul << 0x2E,
+    Unk_30 = 1ul << 0x2F,
+    Unk_31 = 1ul << 0x30,
+    Unk_32 = 1ul << 0x31,
+    Unk_33 = 1ul << 0x32,
+    Unk_34 = 1ul << 0x33,
+    Unk_35 = 1ul << 0x34,
+    Unk_36 = 1ul << 0x35,
+    Unk_37 = 1ul << 0x36,
+    Unk_38 = 1ul << 0x37,
+    Unk_39 = 1ul << 0x38,
+    Unk_3A = 1ul << 0x39,
+    Unk_3B = 1ul << 0x3A,
+    Unk_3C = 1ul << 0x3B,
+    Unk_3D = 1ul << 0x3C,
+    Unk_3E = 1ul << 0x3D,
+    Unk_3F = 1ul << 0x3E,
   }
 
 }
