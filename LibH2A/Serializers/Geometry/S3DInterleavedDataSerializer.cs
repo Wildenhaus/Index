27310ac9822bb8cc62edf6c2e1d1edb0dd621597
Haus <reid@wilden.haus>
2022-06-04 23:47:20 -0400

    Comments

--- LibH2A/Serializers/Geometry/S3DInterleavedDataSerializer.cs
+++ LibH2A/Serializers/Geometry/S3DInterleavedDataSerializer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Numerics;
 using Saber3D.Common;
@@ -52,6 +51,7 @@ namespace Saber3D.Serializers.Geometry
 
       ReadTangents( reader, data );
 
+      // TODO: Not sure what these are. Color?
       if ( Flags.HasFlag( S3DGeometryBufferFlags.Unk_17 ) )
         _ = reader.ReadInt32();
       if ( Flags.HasFlag( S3DGeometryBufferFlags.Unk_18 ) )
@@ -131,64 +131,22 @@ namespace Saber3D.Serializers.Geometry
       if ( isCompressed )
       {
         // Fairly sure this is correct
-        // Broken position?
         var u = reader.ReadInt16().SNormToFloat();
         var v = 1 - reader.ReadInt16().SNormToFloat();
-
-        //Assert( u >= 0 && u <= 1, "UV U coord out of bounds." );
-        //Assert( v >= 0 && v <= 1, "UV V coord out of bounds." );
 
         return new Vector4( u, v, 0, 0 );
       }
       else
       {
         // Fairly sure this is correct
-        // Broken position?
         var u = reader.ReadSingle();
         var v = 1 - reader.ReadSingle();
-
-        //Assert( u >= -2 && u <= 2, "UV U coord out of bounds." );
-        //Assert( v >= -2 && v <= 2, "UV V coord out of bounds." );
 
         return new Vector4( u, v, 0, 0 );
       }
     }
 
     #endregion
-
-    private Vector4 DecompressVectorFromInt16( short w )
-    {
-      /* In common_input.vsh, if the vertex IS compressed, they're unpacking the normal like so:
-       *  xz  = (-1.f + 2.f * frac( float2(1.f/181, 1.f/181.0/181.0) * abs(w))) * float2(181.f/179.f, 181.f/180.f);
-       *  y   = sign(inInt16Value) * sqrt(saturate(1.f - tmp.x*tmp.x - tmp.z*tmp.z));
-       */
-
-      var negativeIdentity = new Vector2( -1.0f );
-
-      var xz = ( negativeIdentity + 2.0f * S3DMath.Frac( new Vector2( 1.0f / 181, 1.0f / 181.0f / 181.0f ) * Math.Abs( w ) ) );
-      xz *= new Vector2( 181.0f / 179.0f, 181.0f / 180.0f );
-
-      var yTmp = S3DMath.Sign( w ) * Math.Sqrt( S3DMath.Saturate( 1.0f - xz.X * xz.X - xz.Y * xz.Y ) );
-
-      var x = ( float ) xz.X;
-      var y = ( float ) yTmp;
-      var z = ( float ) xz.Y;
-
-      return new Vector4( x, y, z, 1 ); // TODO: Should this W be 1?
-    }
-
-    private Vector4 DecompressVectorFromFloat( float w )
-    {
-      /* In common_input.vsh, if the vertex isn't compressed, they're unpacking the normal like so:
-       *  norm = -1.0f + 2.f * float3(1/256.0, 1/256.0/256.0, 1/256.0/256.0/256.0) * w);
-       */
-      var negativeIdentity = new Vector3( -1.0f );
-      var divisor = new Vector3( 0.00390625f, 0.0000152587890625f, 0.000000059604644775390625f );
-      var result = negativeIdentity + 2.0f * S3DMath.Frac( divisor * w );
-
-      return new Vector4( result, 1 ); // TODO: Verify all this math
-    }
-
 
   }
 
