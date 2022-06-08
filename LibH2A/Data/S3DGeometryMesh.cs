3826e9067c56ec9d4c38b810e6ce063627dbe71e
Haus <reid@wilden.haus>
2022-06-08 13:46:41 -0400

    Mark unused S3DGeometryMeshFlags

--- LibH2A/Data/S3DGeometryMesh.cs
+++ LibH2A/Data/S3DGeometryMesh.cs
@@ -25,7 +25,7 @@ namespace Saber3D.Data
   {
     Unk_01 = 1ul << 0x00,
     Unk_02 = 1ul << 0x01,
-    Unk_03 = 1ul << 0x02,
+    Unk_03 = 1ul << 0x02, // Unused
     HasTransformInfo = 1ul << 0x03,
     Unk_05 = 1ul << 0x04,
     Unk_06 = 1ul << 0x05,
@@ -43,9 +43,9 @@ namespace Saber3D.Data
     Unk_12 = 1ul << 0x11,
     Unk_13 = 1ul << 0x12,
     Unk_14 = 1ul << 0x13,
-    Unk_15 = 1ul << 0x14,
-    Unk_16 = 1ul << 0x15,
-    Unk_17 = 1ul << 0x16,
+    Unk_15 = 1ul << 0x14, // Unused
+    Unk_16 = 1ul << 0x15, // Unused
+    Unk_17 = 1ul << 0x16, // Unused
     Unk_18 = 1ul << 0x17,
     Unk_19 = 1ul << 0x18,
     Unk_1A = 1ul << 0x19,
@@ -64,28 +64,28 @@ namespace Saber3D.Data
     Unk_27 = 1ul << 0x26,
     Unk_28 = 1ul << 0x27,
     Unk_29 = 1ul << 0x28,
-    Unk_2A = 1ul << 0x29,
-    Unk_2B = 1ul << 0x2A,
+    Unk_2A = 1ul << 0x29, // Unused
+    Unk_2B = 1ul << 0x2A, // Unused
     Unk_2C = 1ul << 0x2B,
     Unk_2D = 1ul << 0x2C,
     Unk_2E = 1ul << 0x2D,
     Unk_2F = 1ul << 0x2E,
-    Unk_30 = 1ul << 0x2F,
-    Unk_31 = 1ul << 0x30,
-    Unk_32 = 1ul << 0x31,
+    Unk_30 = 1ul << 0x2F, // Unused
+    Unk_31 = 1ul << 0x30, // Unused
+    Unk_32 = 1ul << 0x31, // Unused
     Unk_33 = 1ul << 0x32,
     Unk_34 = 1ul << 0x33,
-    Unk_35 = 1ul << 0x34,
-    Unk_36 = 1ul << 0x35,
-    Unk_37 = 1ul << 0x36,
+    Unk_35 = 1ul << 0x34, // Unused
+    Unk_36 = 1ul << 0x35, // Unused
+    Unk_37 = 1ul << 0x36, // Unused
     Unk_38 = 1ul << 0x37,
     Unk_39 = 1ul << 0x38,
     Unk_3A = 1ul << 0x39,
     Unk_3B = 1ul << 0x3A,
-    Unk_3C = 1ul << 0x3B,
-    Unk_3D = 1ul << 0x3C,
-    Unk_3E = 1ul << 0x3D,
-    Unk_3F = 1ul << 0x3E,
+    Unk_3C = 1ul << 0x3B, // Unused
+    Unk_3D = 1ul << 0x3C, // Unused
+    Unk_3E = 1ul << 0x3D, // Unused
+    Unk_3F = 1ul << 0x3E, // Unused
   }
 
 }
